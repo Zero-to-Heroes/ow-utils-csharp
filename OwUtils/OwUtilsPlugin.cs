5274268de492f868561798e9e69ec371f39191da
sebastientromp <sebastien.tromp@gmail.com>
2023-01-26 19:59:01 +0100

    add delete / download / unzip methods

--- OwUtils/OwUtilsPlugin.cs
+++ OwUtils/OwUtilsPlugin.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Mime;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace OwUtils
 {
@@ -43,7 +47,6 @@ namespace OwUtils
             t.Join();
         }
 
-
         public void flashWindow(string windowName, Action<object, object> callback)
         {
             Thread t = new Thread((ThreadStart)(() =>
@@ -58,6 +61,98 @@ namespace OwUtils
             t.Join();
         }
 
+        public void deleteFileOrFolder(string path, Action<object, object> callback)
+        {
+            Task.Run(() =>
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                else if (Directory.Exists(path))
+                {
+                    ClearFolder(path);
+                    Directory.Delete(path);
+                }
+                callback?.Invoke(null, null);
+            });
+        }
+
+        public void copyFiles(string sourceDir, string targetDir, Action<object, object> callback)
+        {
+            Task.Run(() =>
+            {
+                foreach (var file in Directory.GetFiles(sourceDir))
+                {
+                    File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)), true);
+                }
+                callback?.Invoke(null, null);
+            });
+        }
+
+        public void downloadAndUnzipFile(string fileUrl, string installPath, Action<object, object> callback)
+        {
+            //callback?.Invoke(null, $"Starting to download {fileUrl} and install it to {installPath}");
+            //Thread t = new Thread((ThreadStart)(() =>
+            //{
+            //callback?.Invoke(null, $"Running Task");
+            Task.Run(() =>
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        //callback?.Invoke(null, $"Created web client");
+                        var fileName = $"MelonLoader-{new Random().NextDouble()}.zip";
+                        var zipPath = Path.Combine(Path.GetTempPath(), fileName);
+                        //callback?.Invoke(null, $"Built zipPath {zipPath}");
+                        client.DownloadFile(fileUrl, zipPath);
+                        //callback?.Invoke(null, $"Downloaded file");
+                        var zipArchive = ZipFile.OpenRead(zipPath);
+                        ExtractToDirectory(zipArchive, installPath);
+                        //callback?.Invoke(null, $"Extracted file");
+                    }
+                    callback?.Invoke(true, null);
+                }
+                catch (Exception e)
+                {
+                    callback?.Invoke(null, e.Message);
+                }
+            });
+            //}));
+
+            //t.SetApartmentState(ApartmentState.STA);
+            //t.Start();
+            //t.Join();
+        }
+
+        public void downloadFileTo(string fileUrl, string installPath, string targetFileName, Action<object, object> callback)
+        {
+            Task.Run(() =>
+            {
+                int retriesLeft = 2;
+                while (retriesLeft >= 0)
+                {
+                    try
+                    {
+                        using (var client = new WebClient())
+                        {
+                            var targetPath = Path.Combine(installPath, targetFileName);
+                            client.DownloadFile(fileUrl, targetPath);
+                        }
+                        callback?.Invoke(true, null);
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        retriesLeft--;
+                        continue;
+                    }
+                }
+                callback?.Invoke(null, null);
+            });
+        }
+
         public string ImageToBase64String(System.Drawing.Image imageIn)
         {
             using (var ms = new MemoryStream())
@@ -67,6 +162,34 @@ namespace OwUtils
                 byte[] imageBytes = ms.ToArray();
                 string base64String = Convert.ToBase64String(imageBytes);
                 return base64String;
+            }
+        }
+
+        private void ClearFolder(string directoryPath)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(directoryPath);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        private static void ExtractToDirectory(ZipArchive archive, string destinationDirectoryName)
+        {
+            foreach (ZipArchiveEntry file in archive.Entries)
+            {
+                string completeFileName = Path.Combine(destinationDirectoryName, file.FullName);
+                string directory = Path.GetDirectoryName(completeFileName);
+
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+
+                if (file.Name != "")
+                    file.ExtractToFile(completeFileName, true);
             }
         }
     }
